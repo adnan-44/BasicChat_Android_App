93d35526d9d2f64538f3186d2aea2bdc9246cf69
adnan-44 <mohd.adnan0405@gmail.com>
2022-01-11 02:08:58 +0530

    app: ShowUsersAdapter: Show lastMessage, and it's time on card
    Implemented getAndSetLastMessage() method to get the lastMessage of user's chat and time from database
    Once we get the data snapshot, we can use getMessage() to get message and getTime() to get time
    Also, if lastMessage is sent by current user, then append "YOU:" with text, example
    	You: Hello
    
    * ShowUsersAdapter: call getAndSetLastMessage() method with parameter holder.lastMessage, holder.lastMessageTime so that we can update their values.
    * single_user_item layout: Added lastMessage and lastMessageTime TextView
    * Little fixup, Etc.
    
    Signed-off-by: adnan-44 <mohd.adnan0405@gmail.com>

--- app/src/main/java/com/addy/basicchat/ShowUsersAdapter.java
+++ app/src/main/java/com/addy/basicchat/ShowUsersAdapter.java
@@ -54,7 +54,13 @@ public class ShowUsersAdapter extends RecyclerView.Adapter<ShowUsersAdapter.View
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 // set the user info in userInformation model and update textview
-                holder.username.setText(snapshot.getValue(UserInformation.class).getFull_name());
+                UserInformation userInfo = snapshot.getValue(UserInformation.class);
+                holder.username.setText(userInfo.getFull_name());
+
+                // call getAndSetLastMessage() method to set last message from chats(database)
+                // Pass the lastSentMessage reference (to set the text), also pass user full_name
+                getAndSetLastMessage(position, holder.lastSentMessage,
+                        userInfo.getFull_name(), holder.lastSendMessageTime);
             }
 
             @Override
@@ -67,6 +73,7 @@ public class ShowUsersAdapter extends RecyclerView.Adapter<ShowUsersAdapter.View
             public void onClick(View v) {
                 // Add current user uniqueId and pass them to chatActivity
                 Intent intent = new Intent(activity, ChatActivity.class);
+                intent.putExtra("userUid", chatUsers.get(position).getKey());
                 intent.putExtra("uniqueId", chatUsers.get(position).getValue(String.class));
                 activity.startActivity(intent);
             }
@@ -81,13 +88,42 @@ public class ShowUsersAdapter extends RecyclerView.Adapter<ShowUsersAdapter.View
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         // Views from single_user_item layout
-        TextView username;
+        TextView username, lastSentMessage, lastSendMessageTime;
         CardView single_user_layout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             username = itemView.findViewById(R.id.person_username);
+            lastSentMessage = itemView.findViewById(R.id.last_sent_message);
+            lastSendMessageTime = itemView.findViewById(R.id.last_sent_message_time);
             single_user_layout = itemView.findViewById(R.id.single_user_layout);
         }
+    }
+
+    // Method to get the last message, and set message to last_message textview
+    private void getAndSetLastMessage(int position, TextView lastMessageView, String fullName, TextView lastMessageTimeView){
+        database.child("p2p_chats/"+chatUsers.get(position).getValue(String.class)).limitToLast(1).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snap) {
+                // If snapshot is not null, then get message from snapshot
+                if(snap.exists()){
+                    // Going nesting because message are "pushed" by push() method
+                    for(DataSnapshot data : snap.getChildren()){
+                        // If the sender of message is currently logged user, then add "You: " prefix
+                        Message lastSentMessage = data.getValue(Message.class);
+                        if(lastSentMessage.getSenderUid().equals(fAuth.getCurrentUser().getUid())){
+                            lastMessageView.setText("You: "+lastSentMessage.getMessage());
+                        } else {
+                            lastMessageView.setText(lastSentMessage.getMessage());
+                        }
+                        // Show last sent message time also
+                        lastMessageTimeView.setText(ChatAdapter.getFormattedTime(lastSentMessage.getTime()));
+                    }
+                }
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) { }
+        });
     }
 }
