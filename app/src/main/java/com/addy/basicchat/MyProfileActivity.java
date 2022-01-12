b0175e66737f8963673ce5dd25c1c055c98dd397
adnan-44 <mohd.adnan0405@gmail.com>
2022-01-13 00:52:40 +0530

    app: MyProfileActivity: Allow users to update profile picture
    By using Intent actions, get image which is needed to be uploaded, on successful the intent data will not be null, and if data from intent is not null then store that data in profileImageUri.
    After that call UploadProfileImage() method with profileImageUri as parameter, inside this the image will be uploaded to Firebase storage
    	profile_images -> Uid (where filename will be user's Uid)
    
    And onSuccessfull uploading of image into firebase storage, it'll give downloadable Uri of newly updaloaded image, now call updateUserInfo() method with downloadable imageUri as parameter.
    updateUserInfo() method will create an Map object with key (image_url) and value (downloadable imageUri in string), and will update the
    	all_users_info -> Uid -> image_url (So, that database will have updated Uri to newly uploaded profile image)
    
    * MyProfileActivity: get Image from user using intent actions, upload image using custom uploadProfileImage() method, and on successully uploading, call updateUserInfo() method to update user's profile info with update image_url.
    * activity_my_profile layout: Added ImageView for profile image, and another imageView as (select image_ button
    * gradle : Add dependencies for Firebase Storage
    * Etc.
    
    Signed-off-by: adnan-44 <mohd.adnan0405@gmail.com>

--- app/src/main/java/com/addy/basicchat/MyProfileActivity.java
+++ app/src/main/java/com/addy/basicchat/MyProfileActivity.java
@@ -1,21 +1,35 @@
 package com.addy.basicchat;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyProfileActivity extends AppCompatActivity {
 
@@ -23,10 +37,16 @@ public class MyProfileActivity extends AppCompatActivity {
     private TextView fullName, email, bio;
     private Button edit;
     private Toolbar toolbar;
+    private ImageView profileImage, selectImage;
 
     // Firebase stuff
     private FirebaseAuth fAuth;
     private DatabaseReference database;
+    private StorageReference storage;   // TO work with firebase storage
+    private Uri profileImageUri;
+
+    // Other stuff
+    private static final int IMAGE_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,11 +58,14 @@ public class MyProfileActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         bio = findViewById(R.id.bio);
         edit = findViewById(R.id.edit_profile);
+        profileImage = findViewById(R.id.profile_image);
+        selectImage = findViewById(R.id.select_image);
         toolbar = findViewById(R.id.toolbar);
 
         // Firebase init
         fAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance().getReference("all_users_info");   // data is located at all_users_info
+        storage = FirebaseStorage.getInstance().getReference("profile_images");   // profile images are stored at profile_images
 
         // Toolbar stuff
         setSupportActionBar(toolbar);
@@ -50,6 +73,17 @@ public class MyProfileActivity extends AppCompatActivity {
 
         // Call setProfileInfo() method to get profile values from database and set them in views
         setProfileInfo();
+
+        // Open intent to select image for profile onClick of select_profile_image button
+        selectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open intent in ACTION_PICK to get image URI, which will later be used for profile image
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");  // Because we need only image
+                startActivityForResult(intent, IMAGE_REQUEST_CODE);
+            }
+        });
 
         // Open EditProfileActivity onClick of edit button
         edit.setOnClickListener(new View.OnClickListener() {
@@ -77,6 +111,61 @@ public class MyProfileActivity extends AppCompatActivity {
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) { }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == IMAGE_REQUEST_CODE && data != null && data.getData() != null){
+            // If data is not null, then get Uri from data
+            profileImageUri = data.getData();
+            uploadProfileImage(data.getData());
+        }
+    }
+
+    // Method to take care of uploading image file to firebase storage
+    private void uploadProfileImage(Uri imageUri){
+        // Save the profile image like, profile -> uid
+        storage.child(fAuth.getCurrentUser().getUid()).putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // Get the downloadable Uri from taskSnapshot
+                taskSnapshot.getMetadata().getReference().getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        // Call updateUserInfo to update user info with latest profile image Uri
+                        updateUserInfo(uri);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MyProfileActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    // Method to take care of updating user info database for profile image
+    private void updateUserInfo(Uri downloadableImageUri) {
+        // Create a Map for imageUrl to update user info 
+        Map<String, Object> info = new HashMap<>();
+        info.put("image_url", downloadableImageUri.toString());
+
+        // Users information is store at all_users_info -> Uid
+        database.child(fAuth.getCurrentUser().getUid()).updateChildren(info).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                // If task is successful, then set profile_image with latest image Uri
+                if(task.isSuccessful()){
+                    profileImage.setImageURI(profileImageUri);
+                    Toast.makeText(MyProfileActivity.this, "Profile image updated", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MyProfileActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
         });
     }
 }
