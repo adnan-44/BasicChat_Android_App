a60403d1934128af82175a638c5f2683b619ea27
adnan-44 <mohd.adnan0405@gmail.com>
2022-01-15 00:24:58 +0530

    app: AddNewChatActivity: Search users dynamically on text change in EdiText
    Made searching users more interactive by removing search button and enable search onTextChange() method.
    In simple words, it'll start searching users when text inside "full_name" editText changes (automatically).
    Remove searchUsers(), alreadyKnownUsers() and showUserFoundDialog() method from AddNewChatActivity, because these method are adapted to work with searchUsersByName functionality and moved to ShowAllUsersAdapter (already).
    
    * AddNewChatActivity: removed above mentioned methods, useless imports. Added logic to search users onText change in full_name editText, also clean the list if editText is empty
    * activity_add_new_chat layout: removed search button, decrease "search" TextViews above margin, etc.
    * single_user_info_item layout: Decreased card elements size a little bit.
    
    Signed-off-by: adnan-44 <mohd.adnan0405@gmail.com>

--- app/src/main/java/com/addy/basicchat/AddNewChatActivity.java
+++ app/src/main/java/com/addy/basicchat/AddNewChatActivity.java
@@ -6,21 +6,13 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.Dialog;
-import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -30,14 +22,11 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class AddNewChatActivity extends AppCompatActivity {
 
     // GUI stuff
-    private EditText email;
-    private Button search;
+    private EditText personFullName;
     private Toolbar toolbar;
     private RecyclerView allUsersRecycler;
     private ProgressBar progressBar;
@@ -56,8 +45,7 @@ public class AddNewChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_new_chat);
 
         // Init vars
-        email = findViewById(R.id.person_email);
-        search = findViewById(R.id.search_button);
+        personFullName = findViewById(R.id.person_full_name);
         toolbar = findViewById(R.id.toolbar);
         allUsersRecycler = findViewById(R.id.all_users_recycler);
         progressBar = findViewById(R.id.progress_bar);
@@ -74,17 +62,19 @@ public class AddNewChatActivity extends AppCompatActivity {
         // Set LinearLayout as layout manager for our recyclerView
         allUsersRecycler.setLayoutManager(new LinearLayoutManager(AddNewChatActivity.this));
 
-        // search the given account onclick of search button
-        search.setOnClickListener(new View.OnClickListener() {
+        // Search the users onTextChange in person_full_name editText
+        personFullName.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                // only proceed if user is not searching itself
-                if (!fAuth.getCurrentUser().getEmail().equals(email.getText().toString())) {
-                    //searchUsers();  // Call searchUsers() method to search users
-                    // Search users by their "fullName" field
-                    searchUserByName(email.getText().toString().toLowerCase());
-                }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Whenever user type or remove something in editText, thing method will be automatically called
+                searchUserByName(s.toString().toLowerCase().trim()); // call searchUserByName() method with lowercase string
             }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
         });
     }
 
@@ -103,7 +93,7 @@ public class AddNewChatActivity extends AppCompatActivity {
                 for (DataSnapshot snap : snapshot.getChildren()){
                     // only add users to the list if they have name equal to user's input
                     UserInformation userInfo = snap.getValue(UserInformation.class);
-                    if (userInfo.getFull_name().toLowerCase().contains(name) &&
+                    if (userInfo.getFull_name().toLowerCase().contains(name) && !name.equals("") &&
                              !userInfo.getUid().equals(fAuth.getCurrentUser().getUid())){
                         allUsers.add(userInfo);
                     }
@@ -117,163 +107,5 @@ public class AddNewChatActivity extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError error) { }
         });
-    }
-
-    // Method to take care of searching users within realtime database
-    private void searchUsers(){
-        // check if the provided users exits in platform databases or not, in realtime database, all users are located at /all_user_info location
-        progressBar.setVisibility(View.VISIBLE);    // show loading till data is fetched
-        database.child("all_users_info").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                allUsers.clear();   // clear any previous data
-                for( DataSnapshot snap : snapshot.getChildren()){
-                    // add each user from snapshot into the list
-                    allUsers.add(snap.getValue(UserInformation.class));
-                }
-
-                // now iterate over users list and check if given email exists or not
-                for(UserInformation user : allUsers){
-                    if (user.getEmail().equals(email.getText().toString())){
-                        userFound = true;	// set this true to keep track
-                        // First check current users known contact from root -> p2p_users -> uid
-                        userAlreadyKnown(user);
-                        break;
-                    }
-                }
-
-                // remove the loading and show toast if user not found
-                if(!userFound){
-                    progressBar.setVisibility(View.GONE);
-                    Toast.makeText(AddNewChatActivity.this, "User not found", Toast.LENGTH_LONG).show();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) { }
-        });
-    }
-
-    // Method to check whether that user is already known or not
-    private void userAlreadyKnown(UserInformation user){
-        // If user is not in known list, then only userFoundDialog will be shown
-        database.child("p2p_users/"+fAuth.getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                // Get all the users contact in a list
-                final ArrayList<String> knownUsers = new ArrayList<>();
-                boolean alreadyKnown = false;   // To keep track whether the user is already in contact with current user
-                for (DataSnapshot snap : snapshot.getChildren()){
-                    knownUsers.add(snap.getValue(String.class));
-                    // If user is found in known users list, then set alreadyKnown to true
-                    if(snap.getKey().equals(user.getUid())){
-                        // User is found, set alreadyKnown to true so that userFound dialog will not show
-                        alreadyKnown = true;
-                        progressBar.setVisibility(View.GONE);
-                        Toast.makeText(AddNewChatActivity.this, "User already exists", Toast.LENGTH_LONG).show();
-                        // Stop here, don't check rest of the list
-                        break;
-                    }
-                }
-
-                // show userFoundDialog stuff if user is not in known user
-                if (!alreadyKnown) {
-                    progressBar.setVisibility(View.GONE);
-
-                    // Show Dialog with user information, so that user can add that "user" or cancel operation
-                    showUserFoundDialog(user, AddNewChatActivity.this);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) { }
-        });
-    }
-
-    // Method to take care of user found dialog box, Dialog box is used to show information about user
-    public void showUserFoundDialog(UserInformation userInformation, Context context){
-        // Create and setup dialog for AddNewChatActivity
-        Dialog userFoundDialog = new Dialog(context);
-
-        // Use our custom layout for dialog box and set window height, width parameters as wrap_content
-        userFoundDialog.setContentView(R.layout.user_found_dialog);
-        userFoundDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        userFoundDialog.getWindow().getDecorView().setBackgroundResource(android.R.color.transparent);
-
-        // Get reference to user_found_dialog views and update them with userInformation
-        final TextView fullName = userFoundDialog.findViewById(R.id.full_name);
-        final TextView personEmail = userFoundDialog.findViewById(R.id.person_email);
-        final TextView personBio = userFoundDialog.findViewById(R.id.person_bio);
-        final ImageView personImage = userFoundDialog.findViewById(R.id.person_image);
-        fullName.setText(userInformation.getFull_name());
-        personEmail.setText(userInformation.getEmail());
-        personBio.setText(userInformation.getBio());
-
-        // If getImage_url() returns other value than "default", it means user have an profile image already or using default image
-        if (!userInformation.getImage_url().equals("default")){
-            // Use Glide library to get and set image (using Uri, automatically) in personImage imageView
-            Glide.with(context).load(userInformation.getImage_url()).into(personImage);
-        }
-
-        // click listener for "add" choice button
-        userFoundDialog.findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                progressBar.setVisibility(View.VISIBLE);    // start loading till the process completes
-
-                /* Once a user add another user to their contact/chats, then create a node in p2p_users for both
-                *  the users, like this, take uniqueId = user_uid+other_uid
-                *  p2p_users -> user_uid -> other_uid -> uniqueId and
-                *  p2p_users -> other_uid -> user_uid -> uniqueId
-                */
-                String uniqueId = fAuth.getCurrentUser().getUid() + userInformation.getUid();
-
-                // Create a new child node in (root->p2p_users->user_uid-> uid), so that it'll be available fo known user
-                Map<String, Object> user = new HashMap<>();
-                user.put(userInformation.getUid(), uniqueId);
-
-                database.child("p2p_users").child(fAuth.getCurrentUser().getUid()).updateChildren(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        // if task is completed successfully
-                        if (task.isSuccessful()){
-                            // now save the same uniqueUid for otherUid too
-                            Map<String, Object> otherUser = new HashMap<>();
-                            otherUser.put(fAuth.getCurrentUser().getUid(), uniqueId);
-                            database.child("p2p_users").child(userInformation.getUid()).updateChildren(otherUser).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    // if task is successful then show "user found" toast
-                                    if (task.isSuccessful()){
-                                        Toast.makeText(context, "User added", Toast.LENGTH_SHORT).show();
-                                        progressBar.setVisibility(View.GONE);
-                                        userFoundDialog.dismiss();
-                                        finish();
-                                    } else {
-                                        progressBar.setVisibility(View.GONE);
-                                        Toast.makeText(context, "Failed to add", Toast.LENGTH_LONG).show();
-                                    }
-                                }
-                            });
-                        } else {
-                            progressBar.setVisibility(View.GONE);
-                            Toast.makeText(context, "Failed to add", Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
-            }
-        });
-
-        // dismiss the dialog if "cancel" button is selected
-        userFoundDialog.findViewById(R.id.cancel_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Dismiss the dialog on click
-                userFoundDialog.dismiss();
-            }
-        });
-
-        // show the dialog
-        userFoundDialog.show();
     }
 }
